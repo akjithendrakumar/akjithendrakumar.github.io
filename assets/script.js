6c280cd05ea0487269fd9a3819cd69487a9c5587
jeeKrish <93058399+pjprojdev@users.noreply.github.com>
2026-05-18 22:10:58 -0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const year = document.getElementById("year");
 
+  if (year) {
+    year.textContent = String(new Date().getFullYear());
+  }
+});
